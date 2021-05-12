848df2b75eca7e0bd91dd0060d22888a8517a097
Vitor Hugo <vitor4oliveira@gmail.com>
2021-05-12 18:20:59 -0300

    Ajustes na home e nos arquivos css

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/estilos.css">
-    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="css/home.css">
     <title>Home</title>
 </head>
 
@@ -22,52 +22,104 @@
 
     <MAIN>
         <article>
-        <div class="container mt-5">
+            <div class="container mt-5">
                 <div class="row">
                     <div class="col-12 col-sm-12 col-md-12 col-lg-6">
-                        <h1 class="display-4 text-left" id="titulo_cumprimento">Olá, boa tarde!</h3>
+                        <h1 class="display-4 text-left" id="titulo_cumprimento">Olá, boa tarde!</h1>
                         <img src="img/Covid_Home.png" class="img-fluid" alt="">
                     </div>
                     <div class="col-12 col-sm-12 col-md-12 col-lg-6 mt-1  text-center">
                         <h1 class="display-6 col-sm-12" id="saiba_mais">Saiba tudo sobre o COVID-19</h1>
-                        <h1 class="display-6 col-sm-12" id="subtitulo_cinza" >Como se prevenir, sintomas, estatísticas, vacina e muito mais.</h2>
-                        <a class="btn btn-primary btn-lg col-12" id="botao_verde" href="#" role="button">Ver tópicos</a>
+                        <h1 class="display-6 col-sm-12" id="subtitulo_cinza">Como se prevenir, sintomas, estatísticas, vacina e muito mais.</h2>
+                            <a class="btn btn-primary btn-lg col-12" id="botao_verde" href="#" role="button">Ver tópicos</a>
                     </div>
                 </div>
             </div>
-            <div class="row" style="background-color: gray;">
-                
+            <div class="row mt-5 mb-3" id="div_dados">
+                <div class="col-6 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3" id="card_div">
+                    <div class="card" id="card_conteudo">
+                        <div class="card-body">
+                            <p class="card-text" id="p_card_texto">Óbitos</p>
+                            <p class="card-text" id="p_card_numero">400.000</p>
+                            <img src="img/virus_vermelho.png" class="img-fluid" alt="..." id="img_card" width="40" height="36">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3" id="card_div">
+                    <div class="card" id="card_conteudo">
+                        <div class="card-body">
+                            <p class="card-text" id="p_card_texto">Recuperados</p>
+                            <p class="card-text" id="p_card_numero">400.000</p>
+                            <img src="img/virus_green.png" class="img-fluid" alt="..." id="img_card" width="40" height="36">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3" id="card_div">
+                    <div class="card" id="card_conteudo">
+                        <div class="card-body">
+                            <p class="card-text" id="p_card_texto">Confirmados</p>
+                            <p class="card-text" id="p_card_numero">400.000</p>
+                            <img src="img/virus_vermelho.png" class="img-fluid" alt="..." id="img_card" width="40" height="36">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-3 col-lg-3 col-xl-3 col-xxl-3" id="card_div">
+                    <div class="card" id="card_conteudo">
+                        <div class="card-body">
+                            <p class="card-text" id="p_card_texto">Vacinados</p>
+                            <p class="card-text" id="p_card_numero">400.000</p>
+                            <img src="img/virus_green.png" class="img-fluid" alt="..." id="img_card" width="40" height="36">
+                        </div>
+                    </div>
+                </div>
             </div>
         </article>
         <aside>
-            <header>
-                <h2>Últimas notícias</h2>
-            </header>
-            <section>
-                <img src="">
-                <h3>Titulo Exemplo</h3>
-                <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptatum.</h5>
-                <h6>20/03/2010</h6>
-            </section>
-            <section>
-                <img src="">
-                <h3>Titulo Exemplo</h3>
-                <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptatum.</h5>
-                <h6>20/03/2010</h6>
-            </section>
-            <section>
-                <img src="">
-                <h3>Titulo Exemplo</h3>
-                <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptatum.</h5>
-                <h6>20/03/2010</h6>
-            </section>
-            <section>
-                <img src="">
-                <h3>Titulo Exemplo</h3>
-                <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, voluptatum.</h5>
-                <h6>20/03/2010</h6>
-            </section>
-            <a>Ver tudo</a>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="display-5 text-left" id="titulo_noticias">Últimas noticias</h1>
+                    </div>
+                </div>
+                <div class="row me-5" id="div_noticias">
+                    <div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2">
+                        <img src="img/logo.png" class="img-fluid" alt="" width="200" height="150">
+                    </div>
+                    <div class="col-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4">
+                        <h5 class="">Como se prevenir</h5>
+                        <p class="">Uso de máscaras e higienização das mãos são os principais meios de prevenção.</p>
+                        <p id="data_home">21/03/2021</p>
+                    </div>
+                    <div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2">
+                        <img src="img/logo.png" class="img-fluid" alt="" width="200" height="150">
+                    </div>
+                    <div class="col-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4">
+                        <h5 class="">Como se prevenir</h5>
+                        <p class="">Uso de máscaras e higienização das mãos são os principais meios de prevenção.</p>
+                        <p id="data_home">21/03/2021</p>
+                    </div>
+                    <div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 mt-5">
+                        <img src="img/logo.png" class="img-fluid" alt="" width="200" height="150">
+                    </div>
+                    <div class="col-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 mt-5">
+                        <h5 class="">Como se prevenir</h5>
+                        <p class="">Uso de máscaras e higienização das mãos são os principais meios de prevenção.</p>
+                        <p id="data_home">21/03/2021</p>
+                    </div>
+                    <div class="col-4 col-sm-4 col-md-4 col-lg-2 col-xl-2 col-xxl-2 mt-5">
+                        <img src="img/logo.png" class="img-fluid" alt="" width="200" height="150">
+                    </div>
+                    <div class="col-8 col-sm-8 col-md-8 col-lg-4 col-xl-4 col-xxl-4 mt-5">
+                        <h5 class="">Como se prevenir</h5>
+                        <p class="">Uso de máscaras e higienização das mãos são os principais meios de prevenção.</p>
+                        <p id="data_home">21/03/2021</p>
+                    </div>
+                </div>
+                <div class="text-end">
+                    <a id="link_home" href="">Ver tudo</a>
+                </div>
+                
+            </div>
         </aside>
         <section>
             <section>
