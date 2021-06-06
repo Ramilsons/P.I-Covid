299a9bc7f1916f8c3f071005490b97a5e4edc95d
Vitor Hugo <vitor4oliveira@gmail.com>
2021-06-06 02:38:57 -0300

    Página normas de uso finalizada

--- normas.php
+++ normas.php
@@ -12,6 +12,7 @@
     <!-- Estilos de fonte adicionais -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="./css/estilos.css">
+    <link rel="stylesheet" href="css/normas.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
     <title>Normas</title>
 </head>
@@ -21,99 +22,109 @@
     <?php include "includes/header.php"; ?>
 
     <MAIN>
-        <article>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+        <article id="container">
+            <div class="container">
+                <div class="row text-center mt-3 mb-5">
+                    <h1 class="titulo_principal">Normas de Uso</h1>
+                </div>
+            </div>
             <section>
-                <section>
-                    <img src="">
-                    <h3>Titulo Exemplo</h3>
-                    <h5> sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint </h5>
-                </section>
-                <section>
-                    <img src="">
-                    <h3>Titulo Exemplo</h3>
-                    <h5>sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint </h5>
-                </section>
-                <section>
-                    <img src="">
-                    <h3>Titulo Exemplo</h3>
-                    <h5>sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint </h5>
-                </section>
-                <section>
-                    <img src="">
-                    <h3>Titulo Exemplo</h3>
-                    <h5>sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint </h5>
-                </section>
-                <section>
-                    <img src="">
-                    <h3>Titulo Exemplo</h3>
-                    <h5>sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint </h5>
-                </section>
-                <section>
-                    <img src="">
-                    <h3>Titulo Exemplo</h3>
-                    <h5>sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum.n reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint </h5>
-                </section>
+                <div class="container" id="container_principal">
+                    <div class="row text-center">
+                        <h2 class="titulo_termos">Termos e Condições Gerais de Uso</h2>
+                    </div>
+                    <section>
+                        <div class="text-start">
+                            <img src="img/imagem_normas.png" class="img-fluid text-start" id="imagem1_normas" alt="Retângulo elaborado no mockup">
+                        </div>
+                        <div class="row">
+                            <h3 class="titulo_norma">1.Do objeto</h3>
+                            <h4 class="paragrafo_norma">A plataforma caracteriza-se pela prestação de serviço de informações sobre COVID-19.</h4>
+                            <img src="img/linha_normas.png" id="linha_norma" class="img-fluid text-center mt-4" alt="Linha elaborada no mockup">
+                        </div>
+                    </section>
+                    <section>
+                        <div class="text-end">
+                            <img src="img/imagem_normas.png" class="img-fluid float-end" id="imagem2_normas" alt="Retângulo elaborado no mockup">
+                        </div>
+                        <div class="row mt-4">
+                            <h3 class="titulo_norma">2.Da aceitação</h3>
+                            <h4 class="paragrafo_norma">Ao utilizar a plataforma o usuário aceita integralmente as presentes normas e compromete-se a observá-las, sob o risco de aplicação das penalidades cabíveis.</h4>
+                            <img src="img/linha_normas.png" id="linha_norma" class="img-fluid text-center mt-4" alt="Linha elaborada no mockup">
+                        </div>
+                    </section>
+                    <section>
+                    <div class="text-start">
+                            <img src="img/imagem_normas.png" class="img-fluid float-start" id="imagem3_normas" alt="Retângulo elaborado no mockup">
+                        </div>
+                        <div class="row mt-4">
+                            <h3 class="titulo_norma">3.Do acesso dos Usuários</h3>
+                            <h4 class="paragrafo_norma">Serão utilizadas todas as soluções técnicas à disposição do responsável pela plataforma para permitir o acesso 24 (vinte e quatro) horas por dia.No entanto, a navegação na plataforma ou em alguma de suas páginas poderá ser interrompida , limitada ou suspensa para atualizações, modificações ou qualquer ação necessária ao seu bom funcionamento.</h4>
+                            <img src="img/linha_normas.png" id="linha_norma" class="img-fluid text-center mt-4" alt="Linha elaborada no mockup">
+                        </div>
+                    </section>
+                    <section>
+                    <div class="text-end">
+                            <img src="img/imagem_normas.png" class="img-fluid float-end" id="imagem2_normas" alt="Retângulo elaborado no mockup">
+                        </div>
+                        <div class="row mt-4">
+                            <h3 class="titulo_norma">4.Do Suporte</h3>
+                            <h4 class="paragrafo_norma">Em caso de qualquer dúvida, sugestão ou problema com a utilização da plataforma, o usuário deverá entrar em contato com o suporte através do e-mail:Sistemas@fatecsaoroque.sp.gov.br</h4>
+                            <img src="img/linha_normas.png" id="linha_norma" class="img-fluid text-center mt-4" alt="Linha elaborada no mockup">
+                        </div>
+                    </section>
+                    <section>
+                    <div class="text-start">
+                            <img src="img/imagem_normas.png" class="img-fluid float-start" id="imagem3_normas" alt="Retângulo elaborado no mockup">
+                        </div>
+                        <div class="row mt-4">
+                            <h3 class="titulo_norma">5.Das Responsabilidades</h3>
+                            <h4 class="paragrafo_norma">É de responsábilidade do usuário:
+                                a) a correta utilização dos serviços prestados;
+                                b)pelo cumprimento e respeito do conjunto de regras disposto nesse Termo de Condições de Uso, na respectiva Política de Privacidade e na legislação nacional e internacional.
+                            </h4>
+                            <h4 class="paragrafo_norma">É de responsabilidade da plataforma:
+                                a) indicar as características dos serviços;
+                                b)as informações que foram por ele divulgadas;
+                                c) os conteúdos ou atividades ilicítas praticadas através da sua plataforma;</h4>
+                            <img src="img/linha_normas.png" id="linha_norma" class="img-fluid text-center mt-4" alt="Linha elaborada no mockup">
+                        </div>
+                    </section>
+                    <section>
+                        <div class="text-end">
+                            <img src="img/imagem_normas.png" class="img-fluid float-end"  id="imagem2_normas" alt="Retângulo elaborado no mockup">
+                        </div>
+                        <div class="row mt-4 mb-5">
+                            <h3 class="titulo_norma">6.Dos direitos autorais</h3>
+                            <h4 class="paragrafo_norma">O presente Termo de Uso concede aos usuários para acessar e fazer uso da plataforma e dos serviços por ela disponibilizados.
+                                A estrutura do site, as marcas,logotipos, nomes comerciais,layouts, gráficos e design de interface, imagens, ilustrações, fotografias, apresentações, vídeos, conteúdos escritos,banco de dados e quaisquer outra informação e direitos de propriedades intelectual da razão social XXXXXX, observados os termos da Lei da propriedades industrial (Lei nª9.279/96), Lei de direitos autorais (Leiné9.610/98) e Lei do Software (Lei nª9.609/98),estão devidamente reservados.
+                                Esses Termos de Uso não cede ou transfere ao usuário qualquer direito, de modo que o acesso não gera qualquer direito de propriedade intelectual ao usuário.</h4>
+                            <img src="img/linha_normas.png" id="linha_norma" class="img-fluid text-center mt-4 mb-5" alt="">
+                        </div>
+                    </section>
+                </div>
             </section>
         </article>
-    </MAIN>
-    <footer>
-        <img src="">
-        <nav>
-            <ul>
-                <li><a href="prevencao.php">Prevenção</a></li>
-                <li><a href="sintomas.php">Sintomas</a></li>
-                <li><a href="formas_contagio.php">Formas de Contágio</a></li>
-                <li><a href="vacinas.php">Vacina Contra a COVID-19</a></li>
-            </ul>
-            <ul>
-                <li><a href="fases_contencao.php">Fases de contenção</a></li>
-                <li><a href="estatisticas.php">Estatisticas</a></li>
-                <li><a href="casos_sp.php">Casos em Sp</a></li>
-                <li><a href="contato.php">Contato</a></li>
-            </ul>
-            <ul>
-                <li><a href="duvidas.php">Dúvidas Frequentes</a></li>
-                <li><a href="fakenews.php">Fake News</a></li>
-                <li><a href="normas.php">Normas de uso</a></li>
-                <li><a href="quemsomos.php">Quem somos?</a></li>
-            </ul>
-        </nav>
         <section>
-            <img src="">
-            <img src="">
+            <div class="text-end">
+                <img src="img/virus_gif.gif" class="img-fluid" alt="Imagem de Virús" width="300" height="250" id="img_virus_sintomas" alt="GIF do vírus, um dos requisitos do projeto">
+            </div>
         </section>
-    </footer>
+        <aside>
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <h3 class="p2">Referência Bibliográfica
+                            [Modelo] Termos e condições para site ou app.Publicado por Pensador Jurídico, 2021.Disponível em:
+                            <a href="https://jusbrasilmodelos.jusbrasil.com.br/modelos-pecas/784909844/modelo-termos-e-condicoes-para-site-ou-app.Acesso">JusBrasil</a> em :05 de maio de 2021.
+                        </h3>
+                    </div>
+                </div>
+            </section>
+        </aside>
+    </MAIN>
+    <!-- Footer php -->
+    <?php include "includes/footer.php"; ?>
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 
