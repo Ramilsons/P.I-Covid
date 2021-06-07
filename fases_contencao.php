2445a3bd07f870a82ca14ddbb308d9926bdaa23f
tatiananm <75588923+tatiananm@users.noreply.github.com>
2021-06-07 18:13:34 -0300

    inclusao html e css fases de contencao e html vacinas

--- fases_contencao.php
+++ fases_contencao.php
@@ -2,17 +2,18 @@
 <html lang="pt-br">
 
 <head>
-  <title>Fases de Contenção</title>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <!-- esse cara carrega os ícones do Bootstrap 5 -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
-  <!-- Estilos de fonte adicionais -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link rel="stylesheet" href="./css/estilos.css">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- esse cara carrega os ícones do Bootstrap 5 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <!-- Estilos de fonte adicionais -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="./css/estilos.css">
+    <link rel="stylesheet" href="css/fasescontencao.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <title>Fases de Contenção</title>
 </head>
 
 <body>
@@ -20,69 +21,112 @@
   <?php include "includes/header.php"; ?>
 
   <main>
-    <article>
-      <header>
-        <h2>Fases de Contenção Covid - 19</h2>
-        <h3>Fase Emergencial</h3>
-        <h3>Fase 1 - Vermelha</h3>
-        <h3>Fase 2 - Laranja</h3>
-        <h3>Fase 3 - Amarela</h3>
-        <h3>Fase 4 - Azul</h3>
-        <h3>Fase 5 - Verde</h3>
-      </header>
+    <article> 
+ 
+  <div class="mx-auto" style="width: 700px;">
+   <h1 class="titulo_principal"> Fases de Contenção Covid - 19</h1>
+                <div class="row mx-md-n5">
+
+</div>
+  
+    <section>
+        <div class="row mt-5 mb-5" id="fases">
+          <div class="col-6">
+</div>
+<div class="row" id="fases">
+                      
+  <div class="col-6 float-start">
+  
+  <a class="btn btn-primary btn-lg col-12" id="botao_emergencial" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" role="button"><h5>Fase Emergencial</h5></a>
+  </div>
 
 
-      <section>
-        <h4>Fase Emergencial</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lorem eu ex accumsan sollicitudin.
-          Praesent eu laoreet enim. Ut porttitor nulla sed velit aliquet,
-          eu maximus elit vehicula. Nullam pharetra consectetur consectetur.
-          Nunc faucibus nec enim quis gravida. Sed convallis tellus ante, in venenatis ante laoreet eu. Pellentesque
-          nec
-          elementum arcu.</p>
-      </section>
+  <br>
+  <br>
+  <br>
+
+<div class="col-6 float-start">
+<a class="btn btn-primary btn-lg col-12" id="botao_vermelho" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" role="button"><h5>Fase 1 - Vermelha </h5></a>                     
+</div>
 
 
-      <section>
-        <h4>Fase 1 - Vermelha</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lorem eu ex accumsan sollicitudin.
-          Praesent eu laoreet enim. Ut porttitor nulla sed velit aliquet,
-          eu maximus elit vehicula. Nullam pharetra consectetur consectetur.
-          Nunc faucibus nec enim quis gravida. Sed convallis tellus ante</p>
-      </section>
+<div class="col-6 float-start">
+<a class="btn btn-primary btn-lg col-12" id="botao_laranja" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" role="button"><h5>Fase 2 - Laranja </h5></a>
+</div>
+
+<br>
+<br>
+<br>
+
+<div class="col-6 float-start">
+<a class="btn btn-primary btn-lg col-12" id="botao_amarelo" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" role="button"><h5>Fase 3 - Amarela </h5></a>
+</div>
+
+                            
+<div class="col-6 float-start">
+<a class="btn btn-primary btn-lg col-12" id="botao_verde" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" role="button"><h5>Fase 4 - Verde</h5></a>                         
+</div>
+
+<div class="col-6 float-start">
+<a class="btn btn-primary btn-lg col-12" id="botao_azul" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" role="button"><h5>Fase 5 - Azul</h5></a>
+</div>
+
+<br>
+<br>
+<br>
+<br>
 
 
-      <section>
-        <h4>Fase 2 - Laranja</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lorem eu ex accumsan sollicitudin.
-          Praesent eu laoreet enim. Ut porttitor nulla sed velit aliquet,
-          eu maximus elit vehicula. Nullam pharetra consectetur consectetur.
-          Nunc faucibus nec enim quis gravida. Sed convallis tellus ante </p>
-      </section>
+<div class="row align-items-start">
+  <div class="col">
+  <a class="btn btn-primary btn-lg active" id="botao_emergencial" href="#" role="button"><h5>Fase Emergencial</h5></a></div>
+  <div class="col"><h5 align="justify"> Durante a Fase Emergencial são permitidos; Escritórios em geral e Atividades Administrativas,
+    Repartições de Administração Pública, Telecomunicações, Serviços de Tecnologia da Informação (home office), 
+    Estabelecimentos Comerciais (Comércio em geral), Comércio de Material de Construção, Comércio de Produtos Eletrônicos, 
+    Restaurantes, Bares (drive-thru de 5h às 20h e entrega na casa do comprador ou delivery por 24h.) e Transporte Coletivo (redução dos horários)
+     .</h5></div>
 
-      <section>
-        <h4>Fase 3 - Amarela</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lorem eu ex accumsan sollicitudin.
-          Praesent eu laoreet enim. Ut porttitor nulla sed velit aliquet,
-          eu maximus elit vehicula. Nullam pharetra consectetur consectetur.
-          Nunc faucibus nec enim quis gravida. Sed convallis tellus ante</p>
-      </section>
+     <hr>
+   
+  <div class="col"><a class="btn btn-primary btn-lg active" id="botao_vermelho" href="#" role="button"><h5>Fase 1 - Vermelha </h5></a></div>
+  <div class="col"><h5 align="justify"> Atividades não permitidas na fase vermelha Shopping, Galerias, Estabelecimentos Congêneres,
+     Comércios, Serviços, Consumo Local (Restairante e Similares), Salão de Beleza e Barbearias, Academias de Espostes e Centros de Ginástica,
+    Eventos, Convenções e Atividades Culturais. Atividades Religiosas coletivas. Comércio Varejista de Mercadorias: Lojas de Conveniência
+   (Venda de bebidas alcóolicas das 06h até as 20h)</h5></div>
+</div>
 
-      <section>
-        <h4>Fase 4 - Azul</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lorem eu ex accumsan sollicitudin.
-          Praesent eu laoreet enim. Ut porttitor nulla sed velit aliquet,
-          eu maximus elit vehicula. Nullam pharetra consectetur consectetur.
-          Nunc faucibus nec enim quis gravida. Sed convallis tellus ante</p>
-      </section>
+<hr>
 
-      <section>
-        <h4>Fase 5 - Verde</h4>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut lorem eu ex accumsan sollicitudin.
-          Praesent eu laoreet enim. Ut porttitor nulla sed velit aliquet,
-          eu maximus elit vehicula. Nullam pharetra consectetur consectetur.
-          Nunc faucibus nec enim quis gravida. Sed convallis tellus ante</p>
-      </section>
+<div class="row align-itens-start">
+  <div class="col">
+  <a class="btn btn-primary btn-lg active" id="botao_laranja" href="#" role="button"><h5>Fase 2- Laranja</h5></a></div>
+  <div class="col"><h5 align="justify">São permitidos: Shopping, Galerias, Estabelecimentos Congêneres, Comércios, Serviços, 
+     Consumo Local (Restairante e Similares)(40% da capacidade, 8 horas). Salão de Beleza e Barbearias, Academias de 
+     Espostes e Centros de Ginástica, Eventos, Convenções e Atividades Culturais, (40% da capacidade, 8 horas). Atividades Religiosas coletivas
+    (atividade peritida com adoção dos protocolos geral e setorial específicos.)</h5></div>
+          <hr>
+         s
+  <div class="col"><a class="btn btn-primary btn-lg active" id="botao_amarelo" href="#" role="button"><h5>Fase 3 - Amarela</h5></a></div>
+  <div class="col"><h5 align="justify">São permitidos: Shopping, Galerias, Estabelecimentos Congêneres, Comércios, Serviços,
+  Consumo Local (Restairante e Similares)(40% da capacidade, 8 horas). Salão de Beleza e Barbearias, Academias de 
+     Espostes e Centros de Ginástica, Eventos, Convenções e Atividades Culturais,(40% da capacidade, 10 horas). Atividades Religiosas coletivas
+    (atividade peritida com adoção dos protocolos geral e setorial específicos.)</h5></div>
+</div>
+<hr>
+<div class="row">
+  <div class="col">
+  <a class="btn btn-primary btn-lg active" id="botao_verde" href="#" role="button"><h5>Fase 4 - Verde</h5></a></div>
+  <div class="col"><h5 align="justify">São permitidos: Shopping, Galerias, Estabelecimentos Congêneres, Comércios, Serviços,  
+  Consumo Local (Restairante e Similares)(40% da capacidade, 8 horas). Salão de Beleza e Barbearias, 
+  Academias de Espostes e Centros de Ginástica, Eventos, Convenções e Atividades Culturais,(60% da capacidade, 12 horas).
+  Atividades Religiosas coletivas (atividade peritida com adoção dos protocolos geral e setorial específicos.)</h5></div>
+          <hr>
+  <div class="col"><a class="btn btn-primary btn-lg active" id="botao_azul" href="#" role="button"><h5>Fase 5 - Azul</h5></a></div>
+  <div class="col"><h5 align="justify"> É a última fase onde praticamente tudo volta a funcionar, Shopping, Galerias, Estabelecimentos Congêneres, Comércios, Serviços, 
+    Consumo Local (Restairante e Similares), Salão de Beleza e Barbearias, Academias de Espostes e Centros de Ginástica,
+    Eventos, Convenções e Atividades Culturais. Atividades Religiosas coletivas, etc.</h5></div>
+</div>
+
     </article>
   </main>
   <!-- Footer php -->
--- vacinas.php
+++ vacinas.php
@@ -22,65 +22,73 @@
     <MAIN>
         <article>
 
-            <h1>Vacina Contra a COVID-19</h1>
+
+            <h1>Vacina Contra a COVID-19<span class="badge badge-secondary"></span></h1>
             <p>A CoronaVac é produzida com vírus inativados do novo coronavírus (Sars-CoV-2) para inoculação em
                 humanos.Com a aplicação de duas doses, o sistema imunológico passaria a produzir anticorpos contra o
                 agente causador da COVID-19.</p>
 
             <section>
-                <img src="./img/Ellipse 7.png" alt="vírus laboratório"id="imgvl"/>
-                <h2>1. O vírus é isolado em laboratório.</h2>
-            </section>
-            <section>
-                <img src="./img/Ellipse 4.png" alt="vírus"id="imgv"/>
-                <h3>2. Os cientistas infectam células com o vírus para que ele se multiplique.</h3>
-            </section>
-            <section>
-                <img src="./img/Ellipse 8.png" alt="cientista"id="imgc"/>
-                <h2>3. Os vírus são coletados e inativados por meio de procedimentos químicos para não causarem infecção.
-                </h2>
-            </section>
-            <section>
-                <img src="./img/Ellipse 5.png" alt="vírus seringa"id="imgvs"/>
-                <h3>4. Uma substância chamada adjuvante é adicionada aos vírus inativados e purificados para formular a
-                    vacina.</h3>
-            </section>
-            <section>
-                <img src="./img/Ellipse 6.png" alt="vacina"id="imgva"/>
-                <h2>5. A vacina é aplicada em duas doses para induzir a produção de anticorpos por parte do sistema
-                    imunológico.</h2>
-            </section>
-             <div class="text-end">
-        <img src="img/virus_gif.gif" class="img-fluid" alt="Imagem de Virús" width="300" height="250" id="img_virus_sintomas">
-    </div>
-            <section>
-                <p>Todas as vacinas têm que passar por testes rigorosos para garantir a sua segurança, antes de poderem
-                    ser
-                    introduzidas no programa de vacinação de um país.
-                    <br />Cada vacina em desenvolvimento tem, em primeiro lugar, de ser submetida a exames e avaliações,
-                    para
-                    determinar que antigénio deve ser usado para provocar uma resposta do sistema imunitário. Esta fase
-                    pré-clínica é feita sem testes em humanos. Uma vacina experimental é testada primeiro em animais,
-                    para se
-                    avaliar a sua segurança e potencial para prevenir a doença.<br />Se a vacina desencadear uma
-                    resposta
-                    imunitária, passa a ser testada em ensaios clínicos com humanos em três fases.<br />
-                </p>
-            </section>
-            <footer>
-                <p>
-                    Referência Bibliográfica
-                    <br />Como são as vacinas desenvolvidas? World Health Organization, 2021.Disponível
-                    em:https://www.who.int/pt/news-room/feature-stories/detail/how-are-vaccines-developed .Acesso em: 27
-                    abril
-                    2021.
-                    <br />VACINA CONTRA COVID-19. SP CONTRA O NOVO CORONAVÍRUS, 2021.Disponível em:https://www.saopaulo.sp.gov.br/coronavirus/vacina/.Acesso em: 27 abril 2021.
-                    <br />Unsplash.Photos for everyone,2021.Disponível em:https://unsplash.com/s/photos/covid.Acesso em: 21 maio 2021.<br />
-                </p>
-            </footer>
+                <img src="./img/Ellipse 7.png" class="img-fluid" alt="vírus laboratório" id="imgvl" />
+                <span class="align-middle"></span>
+                <span class="align-text-bottom">1.O vírus é isolado em laboratório.</span>
+        
+                <section>
+
+                    <img src="./img/Ellipse 4.png" class="img-fluid" alt="virus" id="imgv" />
+                    <span class="align-baseline"> 2. Os cientistas infectam células com o vírus para que ele se multiplique.</span>
+                    <div class="text-nowrap bd-highlight" style="width: 8rem;">
+                    </div>
+                </section>
+                <section>
+                    <img src="./img/Ellipse 8.png" class="img-fluid" alt="cientista" id="imgc" />
+                    <span class="align-middle"></span>
+                    <span class="align-text-bottom">3. Os vírus são coletados e inativados por meio de procedimentos químicos para não causarem infecção.</span>
+
+                </section>
+                <section>
+                    <img src="./img/Ellipse 5.png" class="img-fluid" alt="vírus seringa" id="imgvs" />
+                    <span class="align-baseline">4. Uma substância chamada adjuvante é adicionada aos vírus inativados e purificados para formular a
+                        vacina.</span>
+
+                </section>
+                <section>
+                    <img src="./img/Ellipse 6.png" class="img-fluid" alt="vacina" id="imgva" />
+                    <span class="align-middle"></span>
+                    <span class="align-text-bottom">5. A vacina é aplicada em duas doses para induzir a produção de anticorpos por parte do sistema
+                        imunológico.</span>
+
+                </section>
+                <div class="text-end">
+                    <img src="img/virus_gif.gif" class="img-fluid" alt="Imagem de Virús" width="300" height="250" id="img_virus_sintomas">
+                </div>
+                <section>
+                    <p>Todas as vacinas têm que passar por testes rigorosos para garantir a sua segurança, antes de poderem
+                        ser
+                        introduzidas no programa de vacinação de um país.
+                        <br />Cada vacina em desenvolvimento tem, em primeiro lugar, de ser submetida a exames e avaliações,
+                        para determinar que antigénio deve ser usado para provocar uma resposta do sistema imunitário. Esta fase
+                        pré-clínica é feita sem testes em humanos. Uma vacina experimental é testada primeiro em animais,
+                        para se
+                        avaliar a sua segurança e potencial para prevenir a doença.<br />Se a vacina desencadear uma
+                        resposta
+                        imunitária, passa a ser testada em ensaios clínicos com humanos em três fases.<br />
+                    </p>
+                </section>
+                <footer>
+                    <p>
+                        Referência Bibliográfica
+                        <br />Como são as vacinas desenvolvidas? World Health Organization, 2021.Disponível
+                        em:https://www.who.int/pt/news-room/feature-stories/detail/how-are-vaccines-developed .Acesso em: 27
+                        abril
+                        2021.
+                        <br />VACINA CONTRA COVID-19. SP CONTRA O NOVO CORONAVÍRUS, 2021.Disponível em:https://www.saopaulo.sp.gov.br/coronavirus/vacina/.Acesso em: 27 abril 2021.
+                        <br />Unsplash.Photos for everyone,2021.Disponível em:https://unsplash.com/s/photos/covid.Acesso em: 21 maio 2021.<br />
+                    </p>
+                </footer>
         </article>
     </MAIN>
-    
+
     <!-- Footer php -->
     <?php include "includes/footer.php"; ?>
     <script src="js/bootstrap.bundle.min.js"></script>
