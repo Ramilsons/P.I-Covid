2e051b56527c6bb3da14cf6bd3cab301995d84df
Vitor Hugo <vitor4oliveira@gmail.com>
2021-09-09 19:57:41 -0300

    Ajustando site para o item 8 do relatório

--- fases_contencao.php
+++ fases_contencao.php
@@ -34,15 +34,15 @@
 <div class="container">
 <a id="botao_emergencial" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP"><h5>Fase Emergencial</h5></a>
 
-<a id="botao_vermelho" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" ><h5>Fase 1 - Vermelha </h5></a>                     
+<a id="botao_vermelho" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" ><h5>Fase 1</h5></a>                     
 
-<a  id="botao_laranja" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" ><h5>Fase 2 - Laranja </h5></a>
+<a  id="botao_laranja" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP" ><h5>Fase 2</h5></a>
 
-<a id="botao_amarelo" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP"><h5>Fase 3 - Amarela </h5></a>
+<a id="botao_amarelo" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP"><h5>Fase 3</h5></a>
              
-<a id="botao_verde" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP"><h5>Fase 4 - Verde</h5></a>                         
+<a id="botao_verde" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP"><h5>Fase 4</h5></a>                         
 
-<a  id="botao_azul" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP"><h5>Fase 5 - Azul</h5></a>
+<a  id="botao_azul" href="https://www.saopaulo.sp.gov.br/planosp/?utm_source=portal&utm_medium=banner&utm_campaign=PlanoSP"><h5>Fase 5</h5></a>
 </div>
 </section>
 
@@ -59,21 +59,21 @@ Repartições de Administração Pública, Telecomunicações, Serviços de Tecn
 Estabelecimentos Comerciais (Comércio em geral), Comércio de Material de Construção, Comércio de Produtos Eletrônicos, 
 Restaurantes, Bares (drive-thru de 5h às 20h e entrega na casa do comprador ou delivery por 24h.) e Transporte Coletivo (redução dos horários).</p>
 
-  <a id="botao_vermelho"><h5>Fase 1 - Vermelha </h5></a>
+  <a id="botao_vermelho"><h5>Fase 1</h5></a>
   <p id="texto"> Atividades não permitidas na fase vermelha Shopping, Galerias, Estabelecimentos Congêneres,
      Comércios, Serviços, Consumo Local (Restaurante e Similares), Salão de Beleza e Barbearias, Academias de Espostes e Centros de Ginástica,
     Eventos, Convenções e Atividades Culturais. Atividades Religiosas coletivas. Comércio Varejista de Mercadorias: Lojas de Conveniência
    (Venda de bebidas alcóolicas das 06h até as 20h)</p>
  
 
-  <a  id="botao_laranja"><h5>Fase 2- Laranja</h5></a>
+  <a  id="botao_laranja"><h5>Fase 2</h5></a>
  <p id="texto">São permitidos: Shopping, Galerias, Estabelecimentos Congêneres, Comércios, Serviços, 
      Consumo Local (Restaurante e Similares)(40% da capacidade, 8 horas). Salão de Beleza e Barbearias, Academias de 
      Espostes e Centros de Ginástica, Eventos, Convenções e Atividades Culturais, (40% da capacidade, 8 horas). Atividades Religiosas coletivas
     (atividade peritida com adoção dos protocolos geral e setorial específicos.)</p>
 
          
- <a  id="botao_amarelo" ><h5>Fase 3 - Amarela</h5></a>
+ <a  id="botao_amarelo" ><h5>Fase 3</h5></a>
 <p id= "texto" >São permitidos: Shopping, Galerias, Estabelecimentos Congêneres, Comércios, Serviços,
   Consumo Local (Restaurante e Similares)(40% da capacidade, 8 horas). Salão de Beleza e Barbearias, Academias de 
      Espostes e Centros de Ginástica, Eventos, Convenções e Atividades Culturais,(40% da capacidade, 10 horas). Atividades Religiosas coletivas
@@ -81,14 +81,14 @@ Restaurantes, Bares (drive-thru de 5h às 20h e entrega na casa do comprador ou 
 
 
 
-  <a id="botao_verde"><h5>Fase 4 - Verde</h5></a>
+  <a id="botao_verde"><h5>Fase 4</h5></a>
 <p id="texto" > São permitidos: Shopping, Galerias, Estabelecimentos Congêneres, Comércios, Serviços,  
   Consumo Local (Restaurante e Similares)(40% da capacidade, 8 horas). Salão de Beleza e Barbearias, 
   Academias de Espostes e Centros de Ginástica, Eventos, Convenções e Atividades Culturais,(60% da capacidade, 12 horas).
   Atividades Religiosas coletivas (atividade peritida com adoção dos protocolos geral e setorial específicos.)</p>
 
   
-<a id="botao_azul"><h5>Fase 5 - Azul</h5></a>
+<a id="botao_azul"><h5>Fase 5</h5></a>
 <p id="texto"> É a última fase onde praticamente tudo volta a funcionar, Shopping, Galerias, Estabelecimentos Congêneres, Comércios, Serviços, 
     Consumo Local (Restaurante e Similares), Salão de Beleza e Barbearias, Academias de Espostes e Centros de Ginástica,
     Eventos, Convenções e Atividades Culturais. Atividades Religiosas coletivas, etc.</p>
